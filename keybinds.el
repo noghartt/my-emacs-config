9e76ef7ad020b825c22c8ef6ed79e4915c0db09f
Guilherme Ananias <hi@noghartt.dev>
2022-06-19 16:04:25 -0300

    feat(package): use general.el package

--- keybinds.el
+++ keybinds.el
@@ -1,6 +1,14 @@
 ;;; keybinds.el  -*- lexical-binding: t; -*-
 
 ;;; Code:
+(defconst leader-key "SPC"
+  "The leader prefix key for Evil.")
+
+(defconst localleader-key (concat leader-key " m")
+  "The localleader prefix key, for major-mode specific commands.")
+
+;;;; Installing general:
+(use-package general)
 
 ;;;; Installing which-key:
 (use-package which-key
