6496c644c4d2b60f328c2a5011959ba11d447068
Guilherme Ananias <hi@noghartt.dev>
2022-06-19 05:01:03 -0300

    feat(package): add straight.el with initial config

--- init.el
+++ init.el
@@ -12,11 +12,22 @@
 (add-hook 'emacs-startup-hook #'display-startup-time)
 
 ;;;; Initialize package sources:
-(require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org"   . "https://orgmode.org/elpa")
-                         ("elpa"  . "https://elpa.gnu.org/packages")))
+;;;;; Installing straight.el:
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
 
 ;;;; Reduce GC threshold:
 (setq gc-cons-threshold (* 2 1000 1000))
