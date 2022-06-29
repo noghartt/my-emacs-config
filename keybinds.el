0733fcb7c2685f8eebfbda5e173f3262213519e1
Guilherme Ananias <hi@noghartt.dev>
2022-06-29 00:54:59 -0300

    style(keybinds): improve colocations of keybinds variables

--- keybinds.el
+++ keybinds.el
@@ -1,20 +1,15 @@
 ;;; keybinds.el  -*- lexical-binding: t; -*-
 
 ;;; Code:
-(defconst leader-key "SPC"
-  "The leader prefix key for Evil.")
-
-(defconst localleader-key (concat leader-key " m")
-  "The localleader prefix key, for major-mode specific commands.")
-
-;;;; Installing general:
-(use-package general)
 
 ;;;; Installing which-key:
 (use-package which-key
   :init (which-key-mode)
   :config
   (setq which-key-idle-delay 0.3))
+
+;;;; Installing general:
+(use-package general)
 
 ;;;; Installing evil:
 (use-package evil
@@ -25,17 +20,14 @@
   (setq evil-default-cursor 'box
         evil-insert-state-cursor 'box))
 
-(use-package evil-collection
-  :after evil
-  :init
-  (evil-collection-init))
+;;;; Keybinds:
+(defconst leader-key "SPC"
+  "The leader prefix key for Evil.")
+(defconst localleader-key "SPC m"
+  "The localleader prefix key, for major-mode specific commands.")
 
-;;;; Setup keybinds:
-
-;;;;; Evil keybinds:
 (general-create-definer leader-def
   :prefix leader-key)
-
 (general-create-definer localleader-def
   :prefix localleader-key)
 
